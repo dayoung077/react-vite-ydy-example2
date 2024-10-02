af99985c1e2e6be423d86b44abef6d707beb61cf
WIN-J2TB9CU2OL5\ICT02_15 <yoonda000@gmail.com>
2024-10-02 12:45:02 +0900

    라우팅 기본 완료

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import BootStrapButton from './BootstrapExample'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import BoardPage from './pages/BoardPage'
+import ProfilePage from './pages/ProfilePage'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/board" element={<BoardPage />}></Route>
+          <Route path="/profile" element={<ProfilePage />}></Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ProfilePage = () => {
+  return (
+    <div>
+      <h1 class="text-center mt-5">프로필 페이지 입니다.</h1>
+    </div>
+  )
+}
+
+export default ProfilePage
