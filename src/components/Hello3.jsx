fd540f6c0cbf7761519f8ead0c5fed9eb4f4835a
WIN-J2TB9CU2OL5\ICT02_15 <yoonda000@gmail.com>
2024-10-04 13:08:40 +0900

    유즈 스테이트 예제

--- src/components/Hello3.jsx
+++ src/components/Hello3.jsx
@@ -4,9 +4,9 @@ export const Hello3 = ({ name, color, isSpecial, isName }) => {
   return (
     // prettier-ignore
     <div style={{color : color}}>
-      이름 : {isName ? name : null} <br/>
+      이름 : {isName ? name : null} <br/> 
       색상 : {color} <br/>
-      {isSpecial ? <b>*</b> : null}
+      {isSpecial ? <b>******</b> : null}
     </div>
   )
 }
