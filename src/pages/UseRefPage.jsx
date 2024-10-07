d14e709702e5361630cc95a59f67e046f2c68a3f
WIN-J2TB9CU2OL5\ICT02_15 <yoonda000@gmail.com>
2024-10-07 14:13:20 +0900

    성적표2 개선 (최종)

--- src/pages/UseRefPage.jsx
+++ src/pages/UseRefPage.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import UseRefEx1 from '../components/UseRefex1'
 
 const UseRefPage = () => {
+  //prettier-ignore
   return (
     <div className="text-center mt-5">
       <UseRefEx1 />
     </div>
-  )
+  );
 }
 
 export default UseRefPage
--- src/route/Routes.jsx
+++ src/route/Routes.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import HomePage from '../pages/HomePage'
 import BoardPage from '../pages/BoardPage'
-import ProfilePage from './../pages/ProfilePage'
-import PropsPage from '../pages/propsPage'
+import ProfilePage from '../pages/ProfilePage'
+import PropsPage from '../pages/PropsPage'
 import ThreePage from '../pages/ThreePage'
 import CounterPage from '../pages/CounterPage'
 import InputPage from '../pages/InputPage'
 import GradePage from '../pages/GradePage'
-import { UseRefPage } from './../pages/UseRefPage'
+import UseRefPage from '../pages/UseRefPage'
 
 // React Routes, Route 사용 시 배열로 관리하기
 const routes = [
@@ -16,49 +16,41 @@ const routes = [
     element: <HomePage />,
     title: 'Home',
   },
-
   {
     path: '/board',
     element: <BoardPage />,
     title: 'Board',
   },
-
   {
     path: '/profile',
     element: <ProfilePage />,
     title: 'Profile',
   },
-
   {
     path: '/props',
     element: <PropsPage />,
     title: '프롭스',
   },
-
   {
     path: '/three',
     element: <ThreePage />,
-    title: '조건부 랜더링',
+    title: '조건부랜더링',
   },
-
   {
     path: '/usestate',
     element: <CounterPage />,
-    title: 'useState 연습',
+    title: 'useState연습',
   },
-
   {
     path: '/input',
     element: <InputPage />,
-    title: 'input 연습',
+    title: 'input연습',
   },
-
   {
     path: '/grade',
     element: <GradePage />,
     title: '성적표',
   },
-
   {
     path: '/useref',
     element: <UseRefPage />,
