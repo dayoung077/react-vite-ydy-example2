4a935eb18e9b800077a5a10f0cbc05666eb4d44e
WIN-J2TB9CU2OL5\ICT02_15 <yoonda000@gmail.com>
2024-10-07 12:19:45 +0900

    성적표2 완료

--- src/pages/GradePage.jsx
+++ src/pages/GradePage.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Grade from './../components/Grade'
+import Grade2 from '../components/Grade2'
 
 const GradePage = () => {
   // prettier-ignore
   return (
     <div className="text-center mt-5">
       <Grade />
+
+      <hr />
+      
+      <Grade2 />
     </div>
   )
 }
