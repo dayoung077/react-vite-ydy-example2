dcaeb18c2407c6f12ea5ab2ffce019792626307f
WIN-J2TB9CU2OL5\ICT02_15 <yoonda000@gmail.com>
2024-10-08 10:15:23 +0900

    CreateUser

--- src/components/UseRefEx2.jsx
+++ src/components/UseRefEx2.jsx
@@ -6,10 +6,10 @@ import React, { useRef } from 'react'
 // 프로젝트에서도 가끔씩 Dom을 직접 선택해야 하는 상황이 발생
 // 예를 들어서 특정 엘리먼트의 크기를 가져와야 한다든지,
 // 스크롤바 위치를 가져오거나 설정해야 된다든지,
-// 또는 포커스를 설정해 줘야 된다든지 등 정말 다양한 상황이 있겠죠.
-// 추가적으로 Video.js, JWPlayer 같은 HTML5 Video 관련 라이브러리,
-// 또는 D3, chart.js 같은 그래프 관련 라이브러리 등의 외부 라이브러리를 사용해야 할
-// 때에도 특정 DOM에다 적용하기 때문에 DOM 객지를 useRef()로 가져옴
+// 또는 포커스를 설정해 줘야 된다든지 등 정말 다양한 상황이 있다.
+// 추가적으로 Video.js, JWPlayer 같은 HTML5 Video 관련 라이브러리
+// 또는 D3, chart.js 같은 그래프 관련 라이브러리 등의 외부 라이브러리를 사용해야 할 때에도
+// 특정 Dom에다 적용하기 때문에 DOM 객지를 useRef()로 가져온다.
 
 const UseRefEx2 = () => {
   const nameRef = useRef()
