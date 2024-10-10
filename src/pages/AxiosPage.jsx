260fc3a7d4b88252c9b1de24aa9a3812b5d1a6b2
WIN-J2TB9CU2OL5\ICT02_15 <yoonda000@gmail.com>
2024-10-10 09:54:38 +0900

    Axios 예제

---
+++ src/pages/AxiosPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AxiosPage = () => {
+  return (
+    <div>
+      <div class="text-center mt-5">
+        <AxiosGet></AxiosGet>
+      </div>
+    </div>
+  )
+}
+
+export default AxiosPage
